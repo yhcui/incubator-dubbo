a437bc7ba24b1a0980f979248b44f64f0458ea69
cuiyh9 <cuiyh9@ziroom.com>
2019-01-30 16:36:49 +0800

    add: 添加个人注释

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/RandomLoadBalance.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/RandomLoadBalance.java
@@ -24,6 +24,18 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
+ *
+ * RandomLoadBalance 是加权随机算法的具体实现，它的算法思想很简单。
+ * 假设我们有一组服务器 servers = [A, B, C]，他们对应的权重为 weights = [5, 3, 2]，权重总和为10。
+ * 现在把这些权重值平铺在一维坐标值上，[0, 5) 区间属于服务器 A，[5, 8) 区间属于服务器 B，[8, 10) 区间属于服务器 C。
+ * 接下来通过随机数生成器生成一个范围在 [0, 10) 之间的随机数，然后计算这个随机数会落到哪个区间上。
+ * 比如数字3会落到服务器 A 对应的区间上，此时返回服务器 A 即可。
+ * 权重越大的机器，在坐标轴上对应的区间范围就越大，因此随机数生成器生成的数字就会有更大的概率落到此区间内。
+ * 只要随机数生成器产生的随机数分布性很好，在经过多次选择后，每个服务器被选中的次数比例接近其权重比例。
+ * 比如，经过一万次选择后，服务器 A 被选中的次数大约为5000次，服务器 B 被选中的次数约为3000次，服务器 C 被选中的次数约为2000次。
+ * 缺点:
+ * 当调用次数比较少时，Random 产生的随机数可能会比较集中，此时多数请求会落到同一台服务器上
+ * 这个缺点并不是很严重，多数情况下可以忽略。RandomLoadBalance 是一个简单，高效的负载均衡实现，因此 Dubbo 选择它作为缺省实现
  * random load balance.
  */
 public class RandomLoadBalance extends AbstractLoadBalance {
@@ -34,6 +46,8 @@ public class RandomLoadBalance extends AbstractLoadBalance {
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
         // Number of invokers
         int length = invokers.size();
+
+        // 所有的inovker权重是相同的标识
         // Every invoker has the same weight?
         boolean sameWeight = true;
         // the weight of every invokers
@@ -43,12 +57,19 @@ public class RandomLoadBalance extends AbstractLoadBalance {
         weights[0] = firstWeight;
         // The sum of weights
         int totalWeight = firstWeight;
+
+        // 下面这个循环有两个作用，第一是计算总权重 totalWeight，
+        // 第二是检测每个服务提供者的权重是否相同--即每个服务提供者权重值一模一样
         for (int i = 1; i < length; i++) {
             int weight = getWeight(invokers.get(i), invocation);
             // save for later use
             weights[i] = weight;
-            // Sum
+
+            // Sum -  计算总权重 totalWeight
             totalWeight += weight;
+
+            // 检测当前服务提供者的权重与上一个服务提供者的权重是否相同，
+            // 不相同的话，则将 sameWeight 置为 false。
             if (sameWeight && weight != firstWeight) {
                 sameWeight = false;
             }
